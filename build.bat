31d1adccde2476c825e17583ca10e3202a6e59f2
Ren <ren@cuteboxy.com>
2022-11-30 18:58:47 +1000

    Added build batch file for testing cmake.

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
+:: ---
+:: https://stackoverflow.com/questions/15639781/how-to-find-the-qt5-cmake-module-on-windows
+:: ---
 @ECHO OFF
 if exist cmake\ (
   rd /s /q cmake
 )
 
 mkdir cmake&&cd cmake
-cmake ..
+cmake .. -G"Visual Studio 15 2017"
 cd ..
