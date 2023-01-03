4f57a850f2795cd25d175d97c574c72fd783a408
ThirtySomething <developer@derpaul.net>
2023-01-03 18:23:57 +0100

    Intermediate step for project

---
+++ smcore/project.cpp
@@ -0,0 +1,44 @@
+//******************************************************************************
+// Copyright (C) 1999 Jim Wanner and the SourceMonitor team.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//******************************************************************************
+
+#include "project.h"
+#include <filesystem>
+
+namespace smos
+{
+    namespace smcore
+    {
+        //******************************************************************************
+        Project::Project(void)
+        {
+        }
+        //******************************************************************************
+        smos::smcore::Error::ErrorCode Project::loadProject(QString filename)
+        {
+            if (!std::filesystem::exists(filename.toStdString()))
+            {
+                return smos::smcore::Error::ERR_PROJECT_DOES_NOT_EXIST;
+            }
+            return smos::smcore::Error::ERR_EVERYTHING_OK;
+        }
+    }
+}
